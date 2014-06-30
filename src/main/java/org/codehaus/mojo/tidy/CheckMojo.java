20ed5e1ef9995f285dbb50835639fc2bc3fd3960
Stefan Birkner <mail@stefan-birkner.de>
2014-06-30 20:48:07 +0000

    [MOJO-1945] Add tidy:check goal.
    Fail the build if the POM is not tidy. Output a request that tidy:pom
    should be executed.
    
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/tidy-maven-plugin@19842 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/tidy/CheckMojo.java
@@ -0,0 +1,71 @@
+package org.codehaus.mojo.tidy;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+
+/**
+ * Checks that the <code>pom.xml</code> is tidy. Fails the build if <code>mvn tidy:pom</code> would
+ * create a different <code>pom.xml</code> than the current one.
+ */
+@Mojo( name = "check", defaultPhase = LifecyclePhase.VERIFY )
+public class CheckMojo
+    extends AbstractMojo
+{
+    private static final PomTidy POM_TIDY = new PomTidy();
+
+    /**
+     * The Maven Project.
+     */
+    @Component
+    private MavenProject project;
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        try
+        {
+            String pom = Utils.readXmlFile( project.getFile() );
+            String tidyPom = POM_TIDY.tidy( pom );
+            if ( !pom.equals( tidyPom ) )
+            {
+                throw new MojoFailureException(
+                    "The POM violates the code style. Please format it by running `mvn tidy:pom`." );
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+        catch ( XMLStreamException e )
+        {
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+    }
+}
