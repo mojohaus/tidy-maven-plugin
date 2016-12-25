d6e1d69c08badb5c496482d3f66b78c3bfd3fa9b
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2016-12-25 14:28:43 +0100

    Reference a secure XSD location
    Some XML processors may fetch the XSD referenced by the `xsi:schemaLocation`
    attribute. It's better if this happens securely.

--- src/main/java/org/codehaus/mojo/tidy/task/EnsureSingleLineProjectStartTag.java
+++ src/main/java/org/codehaus/mojo/tidy/task/EnsureSingleLineProjectStartTag.java
@@ -30,7 +30,7 @@ class EnsureSingleLineProjectStartTag
 {
     private static final String PROJECT_START_TAG = "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" "
         + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-        + "xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">";
+        + "xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd\">";
 
     @Override
     public String tidyPom( String pom, Format format )
