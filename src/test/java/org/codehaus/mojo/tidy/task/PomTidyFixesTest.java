e0f4f41d0d49d750fbe523e71752b4dd4ba334fc
Frank Weinhold <frank.weinhold@devboost.com>
2022-01-31 17:15:29 +0100

    [#51] fix isStartElementOfScope

---
+++ src/test/java/org/codehaus/mojo/tidy/task/PomTidyFixesTest.java
@@ -0,0 +1,56 @@
+package org.codehaus.mojo.tidy.task;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.codehaus.plexus.util.IOUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class PomTidyFixesTest {
+
+	protected static final String PATH = "fixes/order-and-indent-start-element-of-scope/";
+
+	@Parameters(name = "{0}")
+	public static Iterable<String> tests() {
+		return asList(
+				"property-ending-with-plugin.pom.xml",
+				"property-ending-with-dependency.pom.xml"
+		);
+	}
+
+	@Parameter(0)
+	public String name;
+
+	@Test
+	public void shouldThrowNoError()
+			throws Exception {
+		final String pom = IOUtil.toString(getClass().getResourceAsStream(PATH + name));
+		String tidyPom = new PomTidy().tidy(pom);
+		assertEquals(pom, tidyPom, "nothing to tidy here");
+	}
+
+}
