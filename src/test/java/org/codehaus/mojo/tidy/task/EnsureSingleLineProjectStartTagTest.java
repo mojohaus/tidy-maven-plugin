c923810c152b959a8ff4615c7075d4a7cd77bd3f
Antoine Malliarakis <antoine.malliarakis@gmail.com>
2025-02-23 17:35:39 +0100

    feat: add support for additional project attributes  (#111)

---
+++ src/test/java/org/codehaus/mojo/tidy/task/EnsureSingleLineProjectStartTagTest.java
@@ -0,0 +1,38 @@
+package org.codehaus.mojo.tidy.task;
+
+import javax.xml.stream.XMLStreamException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.codehaus.plexus.util.IOUtil;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test class dedicated to the validation of {@link EnsureSingleLineProjectStartTag}.
+ *
+ * @author Antoine Malliarakis
+ */
+class EnsureSingleLineProjectStartTagTest {
+    @ParameterizedTest(name = "{0}")
+    @ValueSource(
+            strings = {
+                "project-single-line",
+                "project-support-4-1-0-attributes",
+                "project-support-4-1-0-attributes-with-unordered-nodes",
+                "project-support-4-1-0-model-version"
+            })
+    void applyTidying(String name) throws IOException, XMLStreamException {
+        String pom = readPom(name, "pom.xml");
+        String tidyPom = new EnsureSingleLineProjectStartTag().tidyPom(pom, new FormatIdentifier().identifyFormat(pom));
+        assertEquals(readPom(name, "pom-expected.xml"), tidyPom);
+    }
+
+    private String readPom(String test, String filename) throws IOException {
+        InputStream is = getClass().getResourceAsStream(getClass().getSimpleName() + "/" + test + "/" + filename);
+        return IOUtil.toString(is);
+    }
+}
